d0fa1b197d5c5609cd9eb2a3c576d621066041c9
rmtrescato <rmenezestrescato@gmail.com>
2024-03-14 17:53:24 -0300

    refactoring

---
+++ exercicio_lista.py
@@ -0,0 +1,20 @@
+# Criando uma lista de compras
+lista_de_compras = ["Maçã", "Banana", "Leite", "Pão", "Queijo"]
+
+# Adicionando um item à lista
+lista_de_compras.append("Ovos")
+
+# Removendo um item da lista
+lista_de_compras.remove("Banana")
+
+# Exibindo a lista
+print("Lista de Compras:")
+for item in lista_de_compras:
+    print("- " + item)
+    
+
+numero = -1
+while numero <= 0:
+    numero = int(input("Digite um número positivo: "))
+
+print("Você digitou:", numero)
