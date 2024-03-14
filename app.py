965983386ef6329086a967f27851bd936818fe5f
rmtrescato <rmenezestrescato@gmail.com>
2024-03-13 22:25:56 -0300

    Criando opcoes do menu

--- app.py
+++ app.py
@@ -10,11 +10,19 @@ print("""
 """)
 
 print('1. Cadastrar restaurante')
-print('2. Listar Restaurante')
+print('2. Listar Restaurantes')
 print('3. Ativar Restaurante')
 print('4. Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
-print(f'Voce escolheu a opção {opcao_escolhida}')
+opcao_escolhida = int(input('Escolha uma opção: '))
+# opcao_escolhida = int(opcao_escolhida)
 
+if opcao_escolhida == 1:
+    print('Cadastrar Restaurante')
+elif opcao_escolhida == 2:
+    print('Listar Restaurantes')
+elif opcao_escolhida == 3:
+    print('Ativar Restaurante')
+else:
+    print('Encerrando o programa')
 
