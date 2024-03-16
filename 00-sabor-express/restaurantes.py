f5f8f5444664f34c8bd902d5f0f9ff9b6962ed5a
rmtrescato <rmenezestrescato@gmail.com>
2024-03-16 12:34:39 -0300

    Criando classes

---
+++ 00-sabor-express/restaurantes.py
@@ -0,0 +1,11 @@
+class Restaurante:
+   nome = ''
+   categoria = ''
+   ativo = ''
+   
+restaurante_sapore = Restaurante()
+restaurante_carninha = Restaurante()
+
+restaurantes = [restaurante_sapore, restaurante_carninha]
+
+print(restaurantes)
