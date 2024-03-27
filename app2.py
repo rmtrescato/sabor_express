9437ace4372072e0189e98d89ec0d0b048736c9c
rmtrescato <rmenezestrescato@gmail.com>
2024-03-27 11:08:22 -0300

    refatoracao

--- app2.py
+++ app2.py
@@ -1,9 +1,13 @@
 from modelos.restaurantes import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante_sapore = Restaurante('sapore', 'Gourmet')
-restaurante_sapore.receber_avaliacao('Gui', 2)
-restaurante_sapore.receber_avaliacao('Lari', 8)
-restaurante_sapore.receber_avaliacao('Maria', 7.7)
+bebida_suco = Bebida('Suco de Melancia', 5.0, 'Grande')
+prato_paozinho = Prato('Paozinho', 2.00, 'Melhor pao da cidade')
+restaurante_sapore.adicionar_no_cardapio(bebida_suco)
+restaurante_sapore.adicionar_no_cardapio(prato_paozinho)
+
 
 # restaurante_mexicano = Restaurante('Mexican Food', 'Mexicana')
 # restaurante_japones = Restaurante('Ruddy', 'Japonesa')
@@ -12,7 +16,8 @@ restaurante_sapore.receber_avaliacao('Maria', 7.7)
 restaurante_sapore.alternar_estado()
 
 def main():
-    Restaurante.listar_restaurantes()
+    print(bebida_suco)
+    print(prato_paozinho)
     
 if __name__ == '__main__':
     main()
