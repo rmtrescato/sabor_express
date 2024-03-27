9a89499169d65eeba3f400b3b3ab6dbccdf437f1
rmtrescato <rmenezestrescato@gmail.com>
2024-03-27 15:51:43 -0300

    polimorfismo

--- app2.py
+++ app2.py
@@ -4,7 +4,9 @@ from modelos.cardapio.prato import Prato
 
 restaurante_sapore = Restaurante('sapore', 'Gourmet')
 bebida_suco = Bebida('Suco de Melancia', 5.0, 'Grande')
+bebida_suco.aplicar_desconto()
 prato_paozinho = Prato('Paozinho', 2.00, 'Melhor pao da cidade')
+prato_paozinho.aplicar_desconto()
 restaurante_sapore.adicionar_no_cardapio(bebida_suco)
 restaurante_sapore.adicionar_no_cardapio(prato_paozinho)
 
